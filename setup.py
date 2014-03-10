03b4200c3db6b68c57aeaadb30d0d1a9d4c83f9b
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-03-10 20:35:21 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from distutils.core import setup
+
+setup(name='tjjrbot',
+      version='0.1.0',
+      description='Simple but elegant IRC bot.',
+      author='Tuomas Räsänen',
+      author_email='tuomasjjrasanen@tjjr.fi',
+      url='http://tjjr.fi/sw/tjjrbot/',
+      license='GPLv3+',
+      py_modules=['tjjrbot'],
+      platforms=['Linux'],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: POSIX :: Linux",
+          "Topic :: Communications :: Chat :: Internet Relay Chat",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+      ],
+)
